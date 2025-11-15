909f2e0203627db86d3bdaba73a0453d23a42654
lolocaha <sanssansanyc@gmail.com>
2025-11-15 06:19:38 +0000

    Обновленные

--- game.py
+++ game.py
@@ -1,80 +1,64 @@
-import random
 
-class Game15:
-    def __init__(self):
-        self.size = 4
-        self.board = []
-        self.empty = (3, 3)
-        self.moves = 0
-        self.start()
-    def start(self):
-        self.board = []
-        n = 1
-        for i in range(self.size):
-            row = []
-            for j in range(self.size):
-                if i == self.size - 1 and j == self.size - 1:
-                    row.append(0)
+import os
+from game import Game15
+
+class GameUI:
+    def __init__(self, game):
+        self.g = game
+        self.tmp = 0
+    def ochistit_ekran(self):
+        kom = "cls"
+        if os.name != "nt":
+            kom = "clear"
+        os.system(kom)
+    def pokazat_pole(self):
+        p = self.g.poluchit_pole()
+        print("\n=======================")
+        print("     ИГРА 15 (v1)")
+        print("=======================")
+        print("Ходы сделаны:", self.g.poluchit_schetchik_hodov())
+        print()
+        for i in range(len(p)):
+            print("  +----+----+----+----+")
+            stroka = "  |"
+            for j in range(len(p[i])):
+                zn = p[i][j]
+                if zn == 0:
+                    stroka += "    |"
                 else:
-                    row.append(n)
-                    n += 1
-            self.board.append(row)
-        self.empty = (self.size - 1, self.size - 1)
-        self.moves = 0
-    def peremeshat(self, k=1000):
-        dirs = ['verh', 'niz', 'levo', 'pravo']
-        for _ in range(k):
-            d = random.choice(dirs)
-            self.hodit(d)
-    def hodit(self, d):
-        x, y = self.empty
-        if d == 'verh' and x > 0:
-            self.board[x][y], self.board[x - 1][y] = self.board[x - 1][y], self.board[x][y]
-            self.empty = (x - 1, y)
-            self.moves += 1
-            return True
-        if d == 'niz' and x < self.size - 1:
-            self.board[x][y], self.board[x + 1][y] = self.board[x + 1][y], self.board[x][y]
-            self.empty = (x + 1, y)
-            self.moves += 1
-            return True
-        if d == 'levo' and y > 0:
-            self.board[x][y], self.board[x][y - 1] = self.board[x][y - 1], self.board[x][y]
-            self.empty = (x, y - 1)
-            self.moves += 1
-            return True
-        if d == 'pravo' and y < self.size - 1:
-            self.board[x][y], self.board[x][y + 1] = self.board[x][y + 1], self.board[x][y]
-            self.empty = (x, y + 1)
-            self.moves += 1
-            return True
-        return False
-    def proverit_pobedu(self):
-        need = 1
-        for i in range(self.size):
-            for j in range(self.size):
-                if i == self.size - 1 and j == self.size - 1:
-                    if self.board[i][j] != 0:
-                        return False
-                else:
-                    if self.board[i][j] != need:
-                        return False
-                    need += 1
-        return True
-    def poluchit_pole(self):
-        return self.board
-    def poluchit_pustuyu_poziciyu(self):
-        return self.empty
-    def poluchit_schetchik_hodov(self):
-        return self.moves
-    def mozhno_hodit(self, d):
-        x, y = self.empty
-        if d == 'verh' and x > 0:
-            return True
-        if d == 'niz' and x < self.size - 1:
-            return True
-        if d == 'levo' and y > 0:
-            return True
-        if d == 'pravo' and y < self.size - 1:
-            return True
-        return False
+                    stroka += (" " + str(zn).rjust(2, " ") + " |")
+            print(stroka)
+        print("  +----+----+----+----+")
+        print()
+    def pokazat_upravlenie(self):
+        print("Управление:")
+        print(" W - вверх")
+        print(" S - вниз")
+        print(" A - влево")
+        print(" D - вправо")
+        print(" R - рестарт")
+        print(" 0 - выход")
+        print()
+    def poluchit_hod(self):
+        h = input("Введите ход: ").strip().lower()
+
+        while h not in ["w", "a", "s", "d", "r", "0"]:
+            print("Ошибка. Попробуйте снова.")
+            h = input("Введите ход: ").strip().lower()
+        return h
+    def perevesti_napravlenie(self, h):
+        nap = {"w": "verh", "s": "niz", "a": "levo", "d": "pravo"}
+        if h in nap:
+            return nap[h]
+        else:
+            return hе
+    def pokazat_soobshenie(self, t):
+        print("\n" + str(t))
+    def pokazat_pobedu(self):
+        kol = self.g.poluchit_schetchik_hodov()
+        print("\n===================================")
+        print("          ПОБЕДА !!!")
+        print("Вы сделали ходов:", kol)
+        print("===================================")
+    def pokazat_oshibku(self):
+        print("Так ходить нельзя (я сам пробовал).")
--- ui.py
+++ ui.py
@@ -1,62 +1,71 @@
-import os
-from game import Game15
-class GameUI:
-    def __init__(self, game):
-        self.g = game
-        self.tmp = 0
-    def ochistit_ekran(self):
-        kom = "cls"
-        if os.name != "nt":
-            kom = "clear"
-        os.system(kom)
-    def pokazat_pole(self):
-        p = self.g.poluchit_pole()
-        print("\n=======================")
-        print("     ИГРА 15 (v1)")
-        print("=======================")
-        print("Ходы сделаны:", self.g.poluchit_schetchik_hodov())
-        print()
-        for i in range(len(p)):
-            print("  +----+----+----+----+")
-            stroka = "  |"
-            for j in range(len(p[i])):
-                zn = p[i][j]
-                if zn == 0:
-                    stroka += "    |"
-                else:
-                    stroka += (" " + str(zn).rjust(2, " ") + " |")
-            print(stroka)
-        print("  +----+----+----+----+")
-        print()
-    def pokazat_upravlenie(self):
-        print("Управление:")
-        print(" W - вверх")
-        print(" S - вниз")
-        print(" A - влево")
-        print(" D - вправо")
-        print(" R - рестарт")
-        print(" 0 - выход")
-        print()
-    def poluchit_hod(self):
-        h = input("Введите ход: ").strip().lower()
-        while h not in ["w", "a", "s", "d", "r", "0"]:
-            print("Ошибка. Попробуйте снова.")
-            h = input("Введите ход: ").strip().lower()
-        return h
+import random
 
-    def perevesti_napravlenie(self, h):
-        nap = {"w": "verh", "s": "niz", "a": "levo", "d": "pravo"}
-        if h in nap:
-            return nap[h]
-        else:
-            return h
-    def pokazat_soobshenie(self, t):
-        print("\n" + str(t))
-    def pokazat_pobedu(self):
-        kol = self.g.poluchit_schetchik_hodov()
-        print("\n===================================")
-        print("          ПОБЕДА !!!")
-        print("Вы сделали ходов:", kol)
-        print("===================================")
-    def pokazat_oshibku(self):
-        print("Так ходить нельзя (я сам пробовал).")
+class Game15:
+    def __init__(self):
+        self.razmer = 4
+        self.pole = [[0 for _ in range(4)] for _ in range(4)]
+        self.pustaya_poziciya = (3, 3)
+        self.schetchik_hodov = 0
+        self.nachalo()
+    def nachalo(self):
+        k = 1
+        for r in range(self.razmer):
+            for c in range(self.razmer):
+                if r == self.razmer - 1 and c == self.razmer - 1:
+                    self.pole[r][c] = 0
+                else:
+                    self.pole[r][c] = k
+                    k += 1
+        self.pustaya_poziciya = (3, 3)
+        self.schetchik_hodov = 0
+    def peremeshat(self, skolko=1000):
+        vec = ["verh", "niz", "levo", "pravo"]
+        for i in range(skolko):
+            sl = random.randint(0, 3)
+            self.hodit(vec[sl])
+    def hodit(self, kuda):
+        x, y = self.pustaya_poziciya
+        nx, ny = x, y
+        if kuda == "verh":
+            nx = x - 1
+        elif kuda == "niz":
+            nx = x + 1
+        elif kuda == "levo":
+            ny = y - 1
+        elif kuda == "pravo":
+            ny = y + 1
+        if 0 <= nx < self.razmer and 0 <= ny < self.razmer:
+            self.pole[x][y], self.pole[nx][ny] = self.pole[nx][ny], self.pole[x][y]
+            self.pustaya_poziciya = (nx, ny)
+            self.schetchik_hodov += 1
+            return True
+        return False
+    def proverit_pobedu(self):
+        t = 1
+        for r in range(self.razmer):
+            for c in range(self.razmer):
+                if r == self.razmer - 1 and c == self.razmer - 1:
+                    if self.pole[r][c] != 0:
+                        return False
+                else:
+                    if self.pole[r][c] != t:
+                        return False
+                    t += 1
+        return True
+    def poluchit_pole(self):
+        return self.pole
+    def poluchit_pustuyu_poziciyu(self):
+        return self.pustaya_poziciya
+    def poluchit_schetchik_hodov(self):
+        return self.schetchik_hodov
+    def mozhno_hodit(self, kuda):
+        x, y = self.pustaya_poziciya
+        if kuda == "verh":
+            return x > 0
+        if kuda == "niz":
+            return x < self.razmer - 1
+        if kuda == "levo":
+            return y > 0
+        if kuda == "pravo":
+            return y < self.razmer - 1
+        return False
